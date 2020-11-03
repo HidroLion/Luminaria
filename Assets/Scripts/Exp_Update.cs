40a54179d44bf7621ca08760d06ca4fce51dcb75
HidroLion <68381609+HidroLion@users.noreply.github.com>
2020-11-03 09:43:41 -0500

    Revert "Revert "Return""
    This reverts commit a4ce87a540c68285cc97c3ed6f05f9606480244e.

---
+++ Assets/Scripts/Exp_Update.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exp_Update : MonoBehaviour
+{
+    public GameObject User;
+    public Text Level_text;
+    public User_Controller controller;
+
+    public int Level;
+    public int Exp;
+    public int MaxExp;
+    public Slider Slider;
+
+    void Start()
+    {
+        controller = User.GetComponent<User_Controller>();
+        Slider = gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Level = controller.Nivel;
+        Exp = controller.Exp;
+
+        Slider.maxValue = Level * 100;
+        Slider.value = Exp;
+
+        Level_text.text = Level.ToString();
+    }
+}
