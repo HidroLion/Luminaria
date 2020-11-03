3a0ff558d4bdc93fb145212ed3ad5af01748d928
HidroLion <68381609+HidroLion@users.noreply.github.com>
2020-11-03 10:07:54 -0500

    Cambio de Escenas (1)

--- Assets/Scripts/Interfez/Open_Scene.cs
+++ Assets/Scripts/Interfez/Open_Scene.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Open_Scene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string Escena;
 
     // Update is called once per frame
-    void Update()
+    public void AbrirEscena()
     {
-        
+        SceneManager.LoadScene(Escena);
     }
 }
