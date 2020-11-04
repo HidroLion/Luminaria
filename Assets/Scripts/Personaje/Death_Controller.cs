538f643afa9c4720326b76fe06e51a3011021dcb
HidroLion <68381609+HidroLion@users.noreply.github.com>
2020-11-04 09:11:24 -0500

    Finally

--- Assets/Scripts/Personaje/Death_Controller.cs
+++ Assets/Scripts/Personaje/Death_Controller.cs
@@ -8,14 +8,24 @@ public class Death_Controller : MonoBehaviour
     public Transform checkpoint;
     public GameObject player;
     public Text conteoMuertes;
+    public float SaludMax;
 
     public int Muertes;
+    public Stats_Manager GetPlayer;
+
+    void Start()
+    {
+        GetPlayer = GetComponent<Stats_Manager>();
+        SaludMax = GetPlayer.Salud;
+    }
 
     public void Morir()
     {
         player.transform.position = checkpoint.position;
         Muertes++;
         conteoMuertes.text = Muertes.ToString();
+
+        GetPlayer.MostrarSalud(SaludMax);
     }
 
 }
